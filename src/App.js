af24e0c97f15c02f9d2d6c82d43de6b15539940a
Marsel <marsel.solonets@gmail.com>
2020-08-22 23:00:06 +0100

    few countries was added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import {
+  MenuItem,
+  FormControl,
+  Select,
+} from "@material-ui/core";
+import "./App.css";
 
 function App() {
+  const [countries, setCountries] = useState(["Portugal", "Spain", "France", "Ireland", "UK", "USA"]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "app">
+      <div className="app__header">
+      <h1> Covid - 19 Tracker </h1> 
+        <FormControl className = "app_dropdown">
+          <Select variant = "outlined" value = "abc" >
+
+            {
+              countries.map(country => (
+                <MenuItem value = {country} >{country}< /MenuItem> 
+              ))
+            }
+
+            {/* <MenuItem value = "worldwide" > Worldwide < /MenuItem> 
+            <MenuItem value = "worldwide" > Option 2 < /MenuItem> 
+            <MenuItem value = "worldwide" > option3 < /MenuItem> 
+            <MenuItem value = "worldwide" > Option4 < /MenuItem>  */}
+          </Select>
+
+        </FormControl>
+      </div>
+       
     </div>
   );
 }
